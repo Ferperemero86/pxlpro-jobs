a25b514bd8cae7959e276453cf8c870dd417c4e5
Fernando Perez <fernandoperezmerono@gmail.com>
2021-10-19 18:52:32 +0100

    Create mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
 // client-side JS
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuEl = document.querySelector(".mobile-menu");
+  const hamburgerEl = document.querySelector(".hamburger-menu");
+  const closeEl = document.querySelector(".close-icon");
+
+  hamburgerEl.addEventListener("click", function (e) {
+    menuEl.classList.remove("hidden");
+    this.classList.add("hidden");
+  });
+  closeEl.addEventListener("click", function (e) {
+    menuEl.classList.add("hidden");
+  });
+});
